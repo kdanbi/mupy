70ad4b0dd25098c5cfb257052fbf39b0588ee7af
Danbi Kim <danbikim@Danbis-Air.localdomain>
2019-06-12 10:57:42 -0400

    last commit before demo day

---
+++ client/src/components/chat/data.js
@@ -0,0 +1,19 @@
+// let data = [
+//     {'good': ['good','well','awesome']},
+//     {'bad': ['not great','bad','tired']}
+// ]
+
+// let output = [
+//     'How are you',
+//     'Oh no! Why might that be?',
+//     ''
+    
+// ]
+// let counter = 0;
+// if(happy.indexOf(userMsg)) {
+//     botMsg === 'Good to hear you are having a good day!'
+//     counter ++
+// } else if (sad.indexOf(userMsg) {
+//     botMsg === "Oh no! What's wrong? Tell me why you are not having a good day!"
+//     counter ++
+// }
