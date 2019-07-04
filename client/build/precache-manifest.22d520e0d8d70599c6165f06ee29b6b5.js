2b27482743616643e04abfbfb06613b834cd0f15
Danbi Kim <danbikim@Danbis-MacBook-Air.local>
2019-07-04 12:32:37 -0400

    netlify build and toml

---
+++ client/build/precache-manifest.22d520e0d8d70599c6165f06ee29b6b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29ff0c31d65a936553d90ed66519a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d646c3e533657795403",
+    "url": "/static/css/main.b6e66370.chunk.css"
+  },
+  {
+    "revision": "f8617578e9005f244d2d",
+    "url": "/static/js/2.bf1df6b3.chunk.js"
+  },
+  {
+    "revision": "4d646c3e533657795403",
+    "url": "/static/js/main.5e3088e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3d86faba5c39301ef8f0e2ad3ca1c383",
+    "url": "/static/media/chatlogo.3d86faba.svg"
+  },
+  {
+    "revision": "a2fedce90f8cc2df21bfd1ee62fc398e",
+    "url": "/static/media/group.a2fedce9.svg"
+  },
+  {
+    "revision": "ed7d4d6b5676291459189f3c5bbd144f",
+    "url": "/static/media/header-left.ed7d4d6b.svg"
+  },
+  {
+    "revision": "9ee847921596b48f61395d5b816a6bfa",
+    "url": "/static/media/mupy_logo.9ee84792.svg"
+  },
+  {
+    "revision": "337e84c1248e810f7fab1888207cecff",
+    "url": "/static/media/mupy_name.337e84c1.svg"
+  }
+]);
