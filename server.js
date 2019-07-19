965e19f9443df606c28c5ae14e4e5e293f03f2d7
Danbi Kim <danbikim@danbis-air.lan>
2019-07-18 20:54:27 -0400

    chat direct updated

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect(`${URL}/chat/#` +
+        res.redirect(`https://hellomupy.com/chat/#` +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
